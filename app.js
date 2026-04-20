6115dd41cf50e46f2e47fbc0eb73a7dcf5272d00
Francesca Golzio <francescagolzio@gmail.com>
2026-04-20 20:00:49 +0200

    add show route

--- app.js
+++ app.js
@@ -30,6 +30,30 @@ app.get('/movies', (req, res) => {
 
 });
 
+/* SHOW */
+app.post('/movies/:id', (req, res) => {
+
+  const movieId = req.params.id;
+  const sql = 'SELECT * FROM movies WHERE id = ?';
+
+  connection.query(sql, [movieId], (err, results) => {
+
+    if (err) {
+      console.error('Error fetching movie:', err);
+      return res.status(500).json({ error: 'Internal Server Error', message: 'An unexpected error occurred. Please try again later.' });
+    };
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'Movie not found', message: 'The requested movie was not found.' });
+    }
+
+    res.json(results[0]);
+
+  });
+
+});
+
+
 /* Listener */
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
