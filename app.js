60ecb1c359584e49a34c37041040c13dfda677b1
Francesca Golzio <francescagolzio@gmail.com>
2026-04-22 20:55:23 +0200

    clean code

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //🤔 import.meta.env
+/* Movies Router */
 const moviesRouter = require('./routers/moviesRouter');
+app.use('/movies', moviesRouter);
+
 const cors = require('cors');
 
+/* Middleware CORS */
+app.use(cors());
 /* Middleware file statici */
 app.use(express.static('public'));
 /* Middleware for body parser */
 app.use(express.json());
-/* Middleware CORS */
-app.use(cors());
 
 /* SERVER (INDEX) */
 app.get('/', (req, res) => {
   res.json({ 'Welcome': 'Welcome to the Express server!' });
 });
-
-/* Movies Router */
-app.use('/movies', moviesRouter);
 
 
 
@@ -26,6 +26,3 @@ app.use('/movies', moviesRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-/* Movies CRUD */
-app.use('/movies', moviesRouter);
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -9,8 +9,5 @@ moviesRouter.get('/', moviesController.index);
 /* SHOW */
 moviesRouter.get('/:id', moviesController.show);
 
-// Register route handlers
-/* index(); // 🤔⚠️ DA CONTROLLARE, AI suggested
-show(); */
 
 module.exports = moviesRouter;
